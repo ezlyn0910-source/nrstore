8fcb273b2c3a57ac5e557acc21e0592c30f49a76
ezlyn0910-source <ezlyn0910@gmail.com>
2025-10-29 17:19:02 +0800

    Commit Before Balik

--- database/migrations/2025_10_29_083242_create_users_table.php
+++ database/migrations/2025_10_29_083242_create_users_table.php
@@ -18,10 +18,13 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role')->default('customer');
+            $table->enum('status', ['active', 'suspended'])->default('active');
+            $table->string('phone')->nullable();
+            $table->string('last_login_ip')->nullable();
+            $table->timestamp('last_login_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
-
     }
 
     /**
@@ -30,5 +33,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
     }
 };
