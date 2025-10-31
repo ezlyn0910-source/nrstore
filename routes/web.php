6b9f880469ebcab92b98ad46c01dcebece7a9855
ezlyn0910-source <ezlyn0910@gmail.com>
2025-10-31 13:15:02 +0800

    change the manage product folder name

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,12 @@ Route::get('/', function () {
 });
 
 // Product Routes
-Route::get('/manageproducts', [ProductController::class, 'index'])->name('manageproducts.index2');
-Route::get('/manageproducts/{id}', [ProductController::class, 'show'])->name('manageproducts.show');
+Route::get('/manageproduct', [ProductController::class, 'index'])->name('manageproduct.index');
+Route::get('/manageproduct/{id}', [ProductController::class, 'show'])->name('manageproduct.show');
 
 // Category-based filtering
-Route::get('/categories/{category}', [ProductController::class, 'index'])->name('manageproducts.by_category');
-Route::resource('manageproducts', ProductController::class);
+Route::get('/categories/{category}', [ProductController::class, 'index'])->name('manageproduct.by_category');
+Route::resource('manageproduct', ProductController::class);
 
 // Manageuser Routes with additional custom routes
 Route::resource('manageuser', ManageUserController::class);
@@ -43,11 +43,11 @@ Route::middleware(['auth'])->group(function () {
 
 // Variation Routes
 Route::middleware(['auth'])->group(function () {
-    Route::prefix('products/{product}')->group(function () {
-        Route::get('/variations/create', [ProductController::class, 'createVariation'])->name('products.variations.create');
-        Route::post('/variations', [ProductController::class, 'storeVariation'])->name('products.variations.store');
-        Route::get('/variations/{variation}/edit', [ProductController::class, 'editVariation'])->name('products.variations.edit');
-        Route::put('/variations/{variation}', [ProductController::class, 'updateVariation'])->name('products.variations.update');
-        Route::delete('/variations/{variation}', [ProductController::class, 'destroyVariation'])->name('products.variations.destroy');
+    Route::prefix('manageproduct/{product}')->group(function () {
+        Route::get('/variations/create', [ProductController::class, 'createVariation'])->name('manageproduct.variations.create');
+        Route::post('/variations', [ProductController::class, 'storeVariation'])->name('manageproduct.variations.store');
+        Route::get('/variations/{variation}/edit', [ProductController::class, 'editVariation'])->name('manageproduct.variations.edit');
+        Route::put('/variations/{variation}', [ProductController::class, 'updateVariation'])->name('manageproduct.variations.update');
+        Route::delete('/variations/{variation}', [ProductController::class, 'destroyVariation'])->name('manageproduct.variations.destroy');
     });
 });
